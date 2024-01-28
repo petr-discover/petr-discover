7395c3409f0bb87c3fff64a25a57b9d208dce37a
Ryan <leehahnwoong@gmail.com>
2024-01-28 11:22:33 -0800

    fix http

--- backend/cmd/routes/routes.go
+++ backend/cmd/routes/routes.go
@@ -18,7 +18,7 @@ func NewRouter(port string) *chi.Mux {
 	r.Use(middleware.Recoverer)
 	r.Use(cors.Handler(cors.Options{
 		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"*"},
+		AllowedOrigins: []string{"https://*", "http://*"},
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		// AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
