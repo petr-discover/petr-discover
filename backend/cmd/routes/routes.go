73d66bcec7a424beeebf5b30039aff695cdfbfc5
Ryan <leehahnwoong@gmail.com>
2024-01-28 11:15:27 -0800

    fix cors

--- backend/cmd/routes/routes.go
+++ backend/cmd/routes/routes.go
@@ -21,7 +21,7 @@ func NewRouter(port string) *chi.Mux {
 		AllowedOrigins: []string{"*"},
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		// AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		// ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
 		// MaxAge:           300, // Maximum value not ignored by any of major browsers
