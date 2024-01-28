389ede0b850d0a88bcc4a38dedcfddcd2278fb9d
Ryan <leehahnwoong@gmail.com>
2024-01-28 11:16:57 -0800

    fastapi cors fix

--- ai/main.py
+++ ai/main.py
@@ -4,8 +4,16 @@ from yolo import exportData
 import cv2
 import numpy as np
 from io import BytesIO
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/api/v1/ai")
 async def root(file: UploadFile = File(...)):
